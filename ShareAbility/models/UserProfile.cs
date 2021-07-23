5e89bfdca0e1e61a8d8990ce4bd26988548f97c4
Benjamin Golden <b.j.golden@outlook.com>
2021-07-23 10:22:02 -0500

    boiler plate

---
+++ ShareAbility/models/UserProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldenGuitars.models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string FirebaseUserId { get; set; }
+
+      
+    }
+}
+
+
