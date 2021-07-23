ccba837dc0eb956a8331fb4e890eab56bdb727b5
Benjamin Golden <b.j.golden@outlook.com>
2021-07-23 11:20:07 -0500

    boiler plate finished

---
+++ ShareAbility/client/golden-guitars/src/components/ApplicationViews.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Login from "./Login";
+import Register from "./Register";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+
+const ApplicationViews = ({ isLoggedIn }) => {
+  return (
+    <Switch>
+      <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/register">
+          <Register />
+        </Route>
+    </Switch>
+  );
+};
+
+export default ApplicationViews;
