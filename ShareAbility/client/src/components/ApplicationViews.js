319e07ab3cf2f785e1dc9e2d214fedc7efe6b383
Benjamin Golden <b.j.golden@outlook.com>
2021-07-26 16:02:46 -0500

    home page populated

--- ShareAbility/client/src/components/ApplicationViews.js
+++ ShareAbility/client/src/components/ApplicationViews.js
@@ -2,15 +2,16 @@ import React from "react";
 import Login from "./Login";
 import Register from "./Register";
 import { Switch, Route, Redirect } from "react-router-dom";
+import Home from '../components/Home'
 
 
 const ApplicationViews = ({ isLoggedIn }) => {
   return (
     <Switch>
 
-        {/* <Route path="/" exact>
-          {isLoggedIn ? <QuoteList /> : <Redirect to="/login" />}
-        </Route> */}
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Redirect to="/login" />}
+        </Route>
 
       <Route path="/login">
           <Login />
---
+++ ShareAbility/client/src/components/Home.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { Card, CardBody } from "reactstrap";
+import { Link } from 'react-router-dom';
+import { getAllProjects } from "../modules/projectManager";
+import { useHistory } from "react-router";
+    
+const Home = () => {
+    const [projects, setProjects]= useState([]);
+
+    const getAllGuitars = () => {
+        getAllProjects()
+        .then(projects => setProjects(projects))
+    }
+    
+    const handleInputChange = (evt) => {
+        const value = evt.target.value;
+        const key = evt.target.id;
+
+        const projectCopy = { ...projects };
+
+        projectCopy[key] = value;
+        setProjects(projectCopy);
+    };
+
+    useEffect(() => {
+        getAllGuitars()
+    }, []);
+
+    return (
+        <Card className="m-2 p-2 w-50 mx-auto">
+            <CardBody className="m-3">
+                <Link to={`/project/new/`}>
+                <p><b>New Project: </b></p>
+                </Link>
+                            
+                <Link to={`/project/${projects.id}`}> 
+                    <select value={projects.Id} name="projectId" id="projectId" onChange={handleInputChange} className='form-control'>
+                <option value="0">Select a Project</option>
+                {projects.map(p => (
+                    <option key={p.id} value={p.id}>{p.name}</option>
+                ))}
+            </select>
+                </Link>
+                
+                
+            </CardBody>
+        </Card>
+    );
+
+};
+
+export default Home;
